c7aabd23d0f41fd26da57ba20a3c846902b2379c
David Dantas <davidalexandredantas@gmail.com>
2024-02-01 11:38:12 +0000

    tested all php files
    Still not finding every path with ciphered tokens

--- Code/test.sh
+++ Code/test.sh
@@ -3,29 +3,26 @@
 # Set the path to the base directory containing PHP files
 base_dir="../"
 
-# Array of directories to check
+# dirs to check
 php_dirs=("Tests" "WebApps")
 
-# Create a log file to store the runs with non-empty output
-log_file="non_empty_output_runs_ore.txt"
-touch "$log_file"
+log_folder="logs"
+log_file="cifered_ore.txt"
 
-# Iterate through all PHP files in the specified directories and their subdirectories
+# get all php files
 for dir in "${php_dirs[@]}"; do
     php_files_dir="$base_dir$dir"
     
     find "$php_files_dir" -type f -name "*.php" | while read -r php_file; do
-        # Run the Python script with the PHP file and discard the output
         python3 main.py "$php_file" > /dev/null 2>&1
-        # Check output.txt and see if it's different than the string []
+
         output=$(cat "output.txt")
-        empty="[]"
+        empty="[]" # no path found
         if ! test "$output" = "$empty" ; then
-            # Echo what's in the file
             cat "output.txt"
             echo ""
-            # If it is not empty, append the file name to the log file
-            echo "$php_file" >> "$log_file"
+            # found a paht register what file it was
+            echo "$php_file" >> "$base_dir$log_folder/$log_file"
         fi
     done
 done
