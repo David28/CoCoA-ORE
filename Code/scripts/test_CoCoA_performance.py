7733b05901f619767f47dc19e2252f2f696f4c42
David Dantas <davidalexandredantas@gmail.com>
2024-03-12 13:03:33 +0000

    updated testing scripts

--- Code/scripts/test_CoCoA_performance.py
+++ Code/scripts/test_CoCoA_performance.py
@@ -10,7 +10,7 @@ import re
 
 run_count = 5 #number of times to run each file
 master_dir = "../WebAppsComplete/"
-info = [("performance.csv", []), ("performance_e.csv", ["-e"]), ("performance_o.csv", ["-o"])]
+info = [("performance.csv", ["-p"]), ("performance_e.csv", ["-e", "-p"]), ("performance_o.csv", ["-o", "-p"])]
 
 
 def test_file(file_info,flags, timeout=5):
@@ -21,7 +21,7 @@ def test_file(file_info,flags, timeout=5):
     if os.path.isfile(files_dir + file_to_test):
         # Get the output of the main.py with flags -o -d
         try:
-            p = subprocess.run(["python3", "main.py"] + flags+ ["-p"] + [file_to_test], capture_output=True, timeout=timeout)
+            p = subprocess.run(["python3", "main.py"] + flags+ [file_to_test], capture_output=True, timeout=timeout)
         except subprocess.TimeoutExpired:
             result = "Timeout"
             return file_to_test, result, None
@@ -36,19 +36,19 @@ def test_file(file_info,flags, timeout=5):
 def extract_performace_values(output):
     #print("---Preprocessor %s seconds ---" % (time.time() - start_time))
     search = re.compile(r"---Preprocessor (.+) seconds ---")
-    preprocessor_time = float(search.search(output).group(1)) if search.search(output) else None
+    preprocessor_time = float(search.search(output).group(1)) *1000 if search.search(output) else None
     # print("---Lexer %s seconds ---" % (time.time() - start_time))
     search = re.compile(r"---Lexer (.+) seconds ---")
-    lexer_time = float(search.search(output).group(1)) if search.search(output) else None
+    lexer_time = float(search.search(output).group(1))*1000 if search.search(output) else None
     #print("---Translator %s seconds ---" % (time.time() - start_time))
     search = re.compile(r"---Translator (.+) seconds ---")
-    translator_time = float(search.search(output).group(1)) if search.search(output) else None
+    translator_time = float(search.search(output).group(1))*1000 if search.search(output) else None
     #print("---Encryptor %s seconds ---" % (time.time() - start_time))
     search = re.compile(r"---Encryptor (.+) seconds ---")
-    encryptor_time = float(search.search(output).group(1)) if search.search(output) else None
+    encryptor_time = float(search.search(output).group(1))*1000 if search.search(output) else None
     #print("---VD %s seconds ---" % (time.time() - start_time))
     search = re.compile(r"---VD (.+) seconds ---")
-    vd_time = float(search.search(output).group(1)) if search.search(output) else None
+    vd_time = float(search.search(output).group(1))*1000 if search.search(output) else None
     
     #disk usage by the encrypted index read the dump file size filesize.txt
     filesize = os.path.getsize("filesize.txt")
@@ -58,21 +58,27 @@ if __name__ == "__main__":
 
     php_files = []
     for path, subdires, files in os.walk(master_dir):
+        #if "zipec" not in path:
+        #    continue
         for file in files:
             if file.endswith(".php") or file.endswith(".phps"):
                 php_files.append(os.path.join(path, file))
     php_files = [(file, master_dir) for file in php_files]
     
     for output, flags in info:    
+        #clear caches
+        os.system("sync; echo 3 > /proc/sys/vm/drop_caches")
         rows = []
-        rows = [["WebApp","Preprocessor", "Lexer Time", "Translator Time", "Encryptor Time", "VD Time", "Encrypted Index Size", "Success Files Count", "Disk Space", "LOC"]]
-        print("Testing files in: ", master_dir)
+        rows = [["WebApp","Preprocessor Time (ms)", "Lexer Time (ms)", "Translator Time (ms)", "Encryptor Time (ms)", "VD Time (ms)", "Encrypted Index Size (bytes)", "Success Files Count", "Disk Space", "LOC"]]
+        print("Testing files in: ", master_dir + " with flags: " + " ".join(flags))
         count = 0
 
         results = []
         for file in php_files:
+            #clean stdout
+            sys.stdout.flush()
+            sys.stdout.write("\033[K")
             count += 1
-            print("Testing file: " + str(file[0]))
             avgs = []
             for i in range(run_count):
                 result = test_file(file, flags)
@@ -86,7 +92,7 @@ if __name__ == "__main__":
 
                 avgs.append(result)
             if len(avgs) < run_count:
-                print("Error: ", file[0])
+                print(f"Error in file: {file[0]}", end="\r")
                 continue
             avgs = [sum(x)/run_count for x in zip(*avgs)]
             size_of_file = os.path.getsize(file[0])
@@ -95,9 +101,8 @@ if __name__ == "__main__":
                 loc_of_file = len(f.readlines())
             result = [file[0]] + avgs + [1,size_of_file, loc_of_file]
             #remove cientific notation and use comma as decimal separator
-            print(result)
             rows.append(result)
-
+            print(f"Tested file {count}/{len(php_files)}: {file[0]}", end="\r")
 
         #group results by WebApp
         grouped = {}
@@ -113,7 +118,7 @@ if __name__ == "__main__":
         # #create a new csv with the output
         #dont use cientific notation
         for key in grouped:
-            grouped[key] = [f"{x:.20f}".replace(".",",") if x is not None else "" for x in grouped[key][1:]]
+            grouped[key] = [f"{x:.10f}".replace(".",",") if x is not None else "" for x in grouped[key][1:]]
         rows = rows[0:1]
         for key in sorted(grouped.keys(), key=lambda x: x.lower()):
             rows.append([key] + grouped[key])
